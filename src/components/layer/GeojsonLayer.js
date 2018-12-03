8332c87f5fcd73343e37e61cb0f271fac788b527
Aare Puussaar <a.puussaar2@ncl.ac.uk>
2018-12-03 16:15:32 +0000

    :art: added cluster property to geojson layer

--- src/components/layer/GeojsonLayer.js
+++ src/components/layer/GeojsonLayer.js
@@ -18,6 +18,10 @@ export default {
     filter: {
       type: Array,
       default: undefined
+    },
+    cluster: {
+      type: Boolean,
+      default: false
     }
   },
 
@@ -40,14 +44,16 @@ export default {
         try {
           this.map.addSource(this.sourceId, {
             type: 'geojson',
-            data: this.source
+            data: this.source,
+            cluster: this.cluster
           })
         } catch (err) {
           if (this.replaceSource) {
             this.map.removeSource(this.sourceId)
             this.map.addSource(this.sourceId, {
               type: 'geojson',
-              data: this.source
+              data: this.source,
+              cluster: this.cluster
             })
           } else {
             this.$_emitEvent('layer-source-error', { sourceId: this.sourceId, error: err })
